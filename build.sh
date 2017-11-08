ef5ec11d5ea27148c469c02aa107b9eaee025f79
raffaelladevita <devita@ge.infn.it>
2017-11-08 19:08:40 +0100

    added build script and script to launch guis

---
+++ build.sh
@@ -0,0 +1,43 @@
+#!/bin/csh -f
+#=================================================================
+# BUILDING SCRIPT for COATJAVA PROJECT (first maven build)
+# then the documentatoin is build from the sources and commited
+# to the documents page
+#=================================================================
+# Maven Build
+
+if(`filetest -e lib` == '0') then
+    mkdir lib
+endif
+
+# ftCalCalib
+echo "Building ftCalCalib..."
+    cd ftCalCalib
+    mvn install
+    mvn package
+    cp target/FTCalCalib-1.0.jar ../lib/
+    cd ..
+
+# ftCalLedAndCosmics
+echo "Building ftCalLedAndCosmics..."
+    cd ftCalLedAndCosmics
+    mvn install
+    mvn package
+    cp target/FTCalLedAndCosmics-1.0.jar ../lib/
+    cd ..
+
+# ftCalCalib
+echo "Building ftHodoCalib..."
+    cd ftHodoCalib
+    mvn install
+    mvn package
+    cp target/FTHodoCalib-4.0-SNAPSHOT.jar ../lib/
+    cd ..
+
+
+# Finishing touches
+echo ""
+echo "--> Done building....."
+echo ""
+echo "    Usage : build.sh"
+echo ""
